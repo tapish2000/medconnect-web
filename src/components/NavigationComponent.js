1f8a2d7d8983f6df3c11d3c2ae14489a744e951a
rohit-cs18b029 <54976520+rohit-cs18b029@users.noreply.github.com>
2020-10-08 22:42:31 +0530

    Navbar changed (#12)
    * material-kit dependency added
    
    * HeaaderCarouselComponents added
    
    * added some text
    
    * final navbar
    
    Co-authored-by: shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
    Co-authored-by: Tapish Ojha <tapishojha2000@gmail.com>

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -18,19 +18,10 @@ class Navigation extends Component {
 						<Nav.Link href="#">Home</Nav.Link>
 						<Nav.Link href="#map">Stores</Nav.Link>
 						<NavDropdown title="Allopathic" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.1">Generic</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Branded</NavDropdown.Item>
 						</NavDropdown>
-						<NavDropdown title="Ayurvedic" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-						</NavDropdown>
+						<Nav.Link href="#map">Ayurvedic</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
