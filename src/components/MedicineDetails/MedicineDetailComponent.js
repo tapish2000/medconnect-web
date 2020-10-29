39c286f7cc021f62db7f41246483e2c856568b31
tapish2000 <44476667+tapish2000@users.noreply.github.com>
2020-10-29 23:04:18 +0530

    Medicine Details Page Updated
    Issue: #11

--- src/components/MedicineDetails/MedicineDetailComponent.js
+++ src/components/MedicineDetails/MedicineDetailComponent.js
@@ -15,28 +15,6 @@ class MedicineDetails extends Component {
         return (
             <Card className="DetailsCard" id = "cardDesign">
                 <Card.Body className="DetailsCardBody">
-                    {/* <div className = "outer">
-                        <Card.Img className="medicineImage" variant="top" src={this.props.imgsrc} id = "image"/>
-                    </div>
-                    
-                    <div className="outer">
-                        <div className="inner1">
-                            <Card.Title style = {{textAlign:"left",margin:"5%"}}>Medicine Name</Card.Title>
-                            <MedicineInfoComponent style = {{margin:"5%"}} tag = "pain relief" saltName = "paracetamol" medicineWeight = "500gm" medicineQuantity ="15 tablest(s) in a strip" manufacturerName = "Consumer HealthCare Ltd" country="INDIA" amount = "Rs 16.23" />
-                        </div>
-                    </div>
-
-                    <div className="inputTag">
-                        <InputGroup style = {{width:"50%",display:"flex"}}>
-                            <FormControl
-                            placeholder="Quantity"
-                            aria-label="Recipient's username"
-                            aria-describedby="basic-addon2"
-                            type = "number"
-                            />
-                        </InputGroup>
-                        <a href="#" target="_blank" className = "cartbtn"><button className="MyButton">Add to Cart</button></a>
-                    </div> */}
                     <div className="container">
                         <div className="row">
                             <div className="col-sm">
@@ -45,6 +23,8 @@ class MedicineDetails extends Component {
                             <div className="col-sm">
                                 <div className="row">
                                     <Card.Title id="card-head">Medicine Name</Card.Title>
+                                </div>
+                                <div className="row">
                                     <Card.Subtitle id="card-head">Shop Name</Card.Subtitle>
                                     <MedicineInfoComponent style = {{margin:"5%"}} saltName = "paracetamol" medicineWeight = "500gm" manufacturerName = "Consumer HealthCare Ltd" prescription="Yes" amount = "Rs 16.23" />
                                 </div>
@@ -60,8 +40,8 @@ class MedicineDetails extends Component {
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col-sm">
-                                <Card>
+                            <div className="col-10">
+                                <Card style={{width: '100%'}}>
                                     <Card.Body>
                                         <Card.Title>Disclaimer</Card.Title>
                                         <Card.Text className="text-muted">
@@ -73,7 +53,7 @@ class MedicineDetails extends Component {
                                     </Card.Body>
                                 </Card>
                             </div>
-                            <div className="col-sm text-right">
+                            <div className="col-sm text-right my-auto">
                                 <a href="#" role="button" className = "btn btn-outline-primary">Add to Cart</a>
                             </div>
                         </div>
