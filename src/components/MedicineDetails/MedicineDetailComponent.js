d8f819aa234a4a2818ecdd68d889eac1fbba499c
tapish2000 <44476667+tapish2000@users.noreply.github.com>
2020-10-29 22:46:28 +0530

    Medicine Details Page Added

--- src/components/MedicineDetails/MedicineDetailComponent.js
+++ src/components/MedicineDetails/MedicineDetailComponent.js
@@ -43,21 +43,38 @@ class MedicineDetails extends Component {
                                 <Card.Img className="medicineImage" variant="top" src={this.props.imgsrc} id = "image"/>
                             </div>
                             <div className="col-sm">
-                                <Card.Title style = {{textAlign:"left",margin:"5%"}}>Medicine Name</Card.Title>
-                                <MedicineInfoComponent style = {{margin:"5%"}} tag = "pain relief" saltName = "paracetamol" medicineWeight = "500gm" medicineQuantity ="15 tablest(s) in a strip" manufacturerName = "Consumer HealthCare Ltd" country="INDIA" amount = "Rs 16.23" />
+                                <div className="row">
+                                    <Card.Title id="card-head">Medicine Name</Card.Title>
+                                    <Card.Subtitle id="card-head">Shop Name</Card.Subtitle>
+                                    <MedicineInfoComponent style = {{margin:"5%"}} saltName = "paracetamol" medicineWeight = "500gm" manufacturerName = "Consumer HealthCare Ltd" prescription="Yes" amount = "Rs 16.23" />
+                                </div>
+                                <div className="row">
+                                    <select className="form-control" id="quantity">
+                                        <option>Quantity: 1</option>
+                                        <option>Quantity: 2</option>
+                                        <option>Quantity: 3</option>
+                                        <option>Quantity: 5</option>
+                                        <option>Quantity: 10</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-sm">
-                                <InputGroup style = {{width:"50%",display:"flex"}}>
-                                    <FormControl
-                                        placeholder="Quantity"
-                                        aria-label="Recipient's username"
-                                        aria-describedby="basic-addon2"
-                                        type = "number"
-                                    />
-                                </InputGroup>
-                                <a href="#" target="_blank" className = "cartbtn"><button className="MyButton">Add to Cart</button></a>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>Disclaimer</Card.Title>
+                                        <Card.Text className="text-muted">
+                                            The contents of this website are for informational purposes only and not intended to be a substitute
+                                            for professional medical advice, diagnosis, or treatment. Please seek the advice of a physician or other 
+                                            qualified health provider with any questions you may have regarding a medical condition. Do not disregard 
+                                            professional medical advice or delay in seeking it because of something you have read on this website.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div className="col-sm text-right">
+                                <a href="#" role="button" className = "btn btn-outline-primary">Add to Cart</a>
                             </div>
                         </div>
                     </div>
