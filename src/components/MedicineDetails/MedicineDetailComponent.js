5ab52a6dd5d0e47afe2176aa1fdc3ee39f5f6518
tapish2000 <44476667+tapish2000@users.noreply.github.com>
2020-10-29 18:25:58 +0530

    layout of folders and files

---
+++ src/components/MedicineDetails/MedicineDetailComponent.js
@@ -0,0 +1,23 @@
+import React, { useState, Component } from 'react';
+
+class MedicineDetails extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            medName = "Crocin",
+            medMfg = "Glaxosmithkline Consumer Healthcare Ltd",
+            medWeight = "500mg",
+            medSalt = "PARACETAMOL(ACETAMINOPHEN)"
+        };
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MedicineDetails;
