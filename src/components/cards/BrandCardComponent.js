c03ab9ac6a311ea0ab5ebd7fda2d6bdf0ca810b8
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-10-30 22:47:03 +0530

    (Enhancement): Brand logo rounded as UI design
    - logo rounded
    - minor bug fix for shop cards
    
    fixes: #32

--- src/components/cards/BrandCardComponent.js
+++ src/components/cards/BrandCardComponent.js
@@ -3,21 +3,8 @@ import Card from 'react-bootstrap/Card';
 import './CardsComponent.css';
 
 function BrandCardComponent(props) {
-
-    var url = "/medicinedetails/";
-
     return (<>
-        
-        <Card.Link href={url}>
-            <Card className="MyCard" style={{ width: '100%' }}>
-                <Card.Img className="MyImg" variant="top" src={props.imgsrc} />
-                <Card.Body className="MyCardBody">
-                    <Card.Title>Brand Name</Card.Title>
-                    <Card.Text>Some quick example text to build on the card title and make</Card.Text>
-                </Card.Body>
-            </Card>
-        </Card.Link>
-      
+        <img src={props.imgsrc} alt="Avatar" style={{borderRadius:"50%",width:"220px",height:"180px"}}/>
     </>);
 }
 
