a7b7074081ebf03608317e184e24534463098513
Shubhankar Bhadra <42507632+shobhi1310@users.noreply.github.com>
2020-10-30 13:16:17 +0530

    (enhancement): Logo routing to home (#33)
    * material-kit dependency added
    
    * logo routing fixed
    
    Co-authored-by: Tapish Ojha <tapishojha2000@gmail.com>

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -9,7 +9,7 @@ class Navigation extends Component {
 	render() {
 		return (
 			<Navbar collapseOnSelect sticky="top" bg="primary" variant="dark" expand="lg">
-				<Navbar.Brand href="#">
+				<Navbar.Brand href="/">
 					<img src="./assets/logo.png" alt="" width="40" className="d-inline-block align-top" style={{backgroundColor:"white",margin:"10px"}} />
 					MedConnect
 					</Navbar.Brand>
