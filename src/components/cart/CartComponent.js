c02f1feed9ad7b2419532fefba8c145d477fa508
Mir Sameed Ali <meersameed@gmail.com>
2020-11-01 15:03:45 +0530

    Cart Page synced with backend

--- src/components/cart/CartComponent.js
+++ src/components/cart/CartComponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import {Spinner} from 'react-bootstrap';
 import "./CartComponent.css"
 import {connect} from 'react-redux';
 import { MDBRow, MDBCard, MDBCardBody, MDBTooltip, MDBTable, MDBTableBody, MDBTableHead, MDBInput, MDBBtn } from "mdbreact";
@@ -8,7 +10,8 @@ class CartComponent extends Component {
   constructor(props){
     super(props);
     this.state={
-        
+        data:[],
+        loading:true,
         columns: [
           {
             label: '',
@@ -39,40 +42,45 @@ class CartComponent extends Component {
     }
   }
 
-  static getDerivedStateFromProps(props, state){
-    
-        return {
-          data: props.data,
-          ...state
-        }
+  componentDidMount(){
+    axios.get('http://localhost:5000/user/cart/view/5f4a95114a72100017272afe')
+    .then((response)=>{
+     
+      this.setState({data:response.data.cart,loading:false});
+    }).catch((err)=>{
+      console.log(err);
+    })
   }
 
 
 totalSum = (val) => {
-    this.state.sum=0
     this.state.sum += Number(val);
 }
 
-onQuantityChanged=(e,medicine)=>{
-  console.log(e.target.value);
-  this.props.onMedicineQuantityChanged(this.props.data,medicine,e.target.value);
-  }
+// onQuantityChanged=(e,medicine)=>{
+//   console.log(e.target.value);
+//   this.props.onMedicineQuantityChanged(this.props.data,medicine,e.target.value);
+//   }
 
 render() {
+    let sum=0;
 
     const rows = [];
     const { columns, data } = this.state;
 
     data.map(row => {
-        this.totalSum(row.quantity * row.price);
+        sum+=(row.quantity * row.medicine.price);
+        
       return rows.push(
         {
-        'img': <img src={row.image_url} alt="" className="img-fluid z-depth-0 image-cart" />,
-        'name': [<h5 className="mt-3"><strong>{row.name}</strong></h5>, <p className="text-muted">{row.shopName}</p>],
-        'price': `₹ ${row.price}`,
+        'img': <img src={row.medicine.image_url} alt="" className="img-fluid z-depth-0 image-cart" />,
+        'name': [<h5 className="mt-3"><strong>{row.medicine.name}</strong></h5>, <p className="text-muted">{row.shop.name}</p>],
+        'price': `₹ ${row.medicine.price}`,
         'qty':
         <>
-            <select defaultValue={row.quantity} className="mdb-select md-form" style={{ width: "100px" }} onChange={(e)=>this.onQuantityChanged(e,row)}>
+            <select defaultValue={row.quantity} className="mdb-select md-form" style={{ width: "100px" }}
+            //  onChange={(e)=>this.onQuantityChanged(e,row)}
+             >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -80,7 +88,7 @@ render() {
                 <option value="10">10</option>
             </select>
         </>,
-        'amount': <strong>₹ {(row.quantity * row.price)}</strong>,
+        'amount': <strong>₹ {(row.quantity * row.medicine.price)}</strong>,
         'button':
         <a href="#">Remove</a>
         }
@@ -89,7 +97,14 @@ render() {
     });
 
     return (
-    <MDBRow className="my-2" center>
+      (this.state.loading)?(<div className="SpinnerDiv">
+      <Spinner
+        animation="border"
+        variant="info"
+        style={{ margin: '5% auto' }}
+      />
+    </div>):
+    (<MDBRow className="my-2" center>
       <MDBCard className="w-100">
         <MDBCardBody>
           <MDBTable className="product-table">
@@ -98,7 +113,7 @@ render() {
           </MDBTable>
           <div className="row">
               <div className="col-9">
-                <strong>Total Amount: {this.state.sum.toFixed(2)}</strong>
+          <strong>Total Amount: {sum.toFixed(2)}</strong>
               </div>
               <div className="col-sm">
                   <a className="btn btn-outline-primary" role="button" href="#">Book Now</a>
@@ -106,30 +121,30 @@ render() {
           </div>
         </MDBCardBody>
       </MDBCard>
-    </MDBRow>
+    </MDBRow>)
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-      data:state.medicinesInCart
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//       data:state.medicinesInCart
+//   };
+// };
 
-const mapDispatchToProps = dispatch => {
-  return {
-      onMedicineQuantityChanged: (medicineList,medicine,newQuantity) => {
-        let newMedicineList=[...medicineList];
-        for(let i=0;i<newMedicineList.length;i++){
-          if(newMedicineList[i]===medicine){
-            newMedicineList[i].quantity=newQuantity;
-          }
-        }
-        dispatch({type: "MODIFY_CART",medicineList:newMedicineList});
-      }
-  }
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//       onMedicineQuantityChanged: (medicineList,medicine,newQuantity) => {
+//         let newMedicineList=[...medicineList];
+//         for(let i=0;i<newMedicineList.length;i++){
+//           if(newMedicineList[i]===medicine){
+//             newMedicineList[i].quantity=newQuantity;
+//           }
+//         }
+//         dispatch({type: "MODIFY_CART",medicineList:newMedicineList});
+//       }
+//   }
+// };
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartComponent);
+export default (CartComponent);
