925005da192b0de2fa5490d843bc798aa6022953
tapish2000 <44476667+tapish2000@users.noreply.github.com>
2020-11-02 17:49:28 +0530

    Updates

---
+++ precache-manifest.65e4940132d67693aad2485838afcae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8228d5a81502e493a4972532b09dfb",
+    "url": "/medconnect-web/index.html"
+  },
+  {
+    "revision": "505fc4afed498b17b3e4",
+    "url": "/medconnect-web/static/css/2.31452f42.chunk.css"
+  },
+  {
+    "revision": "47eeee9f8ee83f989b45",
+    "url": "/medconnect-web/static/css/main.fbb0de8a.chunk.css"
+  },
+  {
+    "revision": "505fc4afed498b17b3e4",
+    "url": "/medconnect-web/static/js/2.500ef2ca.chunk.js"
+  },
+  {
+    "revision": "45bc4e006fba991bf7a6b8f600ed4277",
+    "url": "/medconnect-web/static/js/2.500ef2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47eeee9f8ee83f989b45",
+    "url": "/medconnect-web/static/js/main.378f9878.chunk.js"
+  },
+  {
+    "revision": "8df4d84fd93c06f90bed",
+    "url": "/medconnect-web/static/js/runtime-main.14d8c35b.js"
+  }
+]);
