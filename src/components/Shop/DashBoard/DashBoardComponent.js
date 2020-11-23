1d4f8d9786c2e76cea99ebda70ba935fc892366d
chirag2706 <cs18b006@iittp.ac.in>
2020-11-23 09:34:15 +0530

    added heading to dashboard

--- src/components/Shop/DashBoard/DashBoardComponent.js
+++ src/components/Shop/DashBoard/DashBoardComponent.js
@@ -18,6 +18,9 @@ class DashBoard extends React.Component{
       render(){
         return (
           <>
+          <div>
+            <h1 className = "block">DashBoard</h1>
+          </div>
             <div className = "BarGraph">
                 <div>
                    <BarGraphForProfit />
