12f241fe4a0b4e09a94aee17ffe7e0db22ff14a0
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-11-24 16:04:58 +0530

    (feat): Footer for Shop
    Fixes: #97

--- src/Shop.js
+++ src/Shop.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SideNavBar from './components/Shop/SideNavBar/Sidebar';
 import MedicineInventory from './components/Shop/MedicineInventory/MedicineInventoryComponent';
+import Footer from './components/Shop/footer/FooterComponent'
 
 function Shop() {
   return (    
@@ -10,6 +11,7 @@ function Shop() {
       <Switch>
           <Route path="/shop/inventory" component={MedicineInventory} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Shop/footer/FooterComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import './FooterComponent.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faYoutube,
+	faFacebook,
+	faTwitter,
+	faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+
+class Footer extends Component {
+	render() {
+		return (
+			<div className="main-footer bg-info" style={{marginLeft:"75px",marginTop:"10px"}}>
+				<div className="container-fluid text-light">
+					<div className="d-flex justify-content-around">
+						<div className="col-md-2"><h5>Quick Links</h5></div>
+						<div className="col-md col-4">
+							<div className="row">
+								<div className="col-12"><Link to={"#"} style={{color:"white",textDecoration:"none"}}>Dashbpard</Link></div>
+								<div className="col-12"><Link to={"#"} style={{color:"white",textDecoration:"none"}}>Inventory</Link></div>
+							</div>
+						</div>
+						<div className="col-md col-4">
+							<div className="row">
+								<div className="col-12"><Link to={"#"} style={{color:"white",textDecoration:"none"}}>Bookings</Link></div>
+								<div className="col-12"><Link to={"#"} style={{color:"white",textDecoration:"none"}}>Profile</Link></div>
+							</div>
+						</div>
+						<div className="col-md col-4">
+						<div className="row">
+								<div className="col-12"><Link to={"#"} style={{color:"white",textDecoration:"none"}}>About Us</Link></div>
+								<div className="col-12"><Link to={"/ayurbrand"} style={{color:"white",textDecoration:"none"}}>Support</Link></div>
+							</div>
+						</div>
+					</div>
+					<div className="d-flex justify-content-around">
+						<div className="social-container">
+							<h5 className="text-white">Connect With Us</h5>
+							<a href="#" className="facebook social">
+								<FontAwesomeIcon icon={faFacebook} size="2x" />
+							</a>
+							<a href="#" className="twitter social">
+								<FontAwesomeIcon icon={faTwitter} size="2x" />
+							</a>
+							<a href="#" className="instagram social">
+								<FontAwesomeIcon icon={faInstagram} size="2x" />
+							</a>
+						</div>
+						<div className="footer-bottom">
+							<p className="text-xs-center">
+								MedConnect &copy;{new Date().getFullYear()} - All Rights Reserved
+							</p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Footer;
