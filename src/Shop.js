8c16ca2c7446e40a6872d8f767a2ada62d1cfd25
rohit-cs18b029 <54976520+rohit-cs18b029@users.noreply.github.com>
2020-11-24 18:27:11 +0530

    datatables added in medicine inventory

--- src/Shop.js
+++ src/Shop.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SideNavBar from './components/Shop/SideNavBar/Sidebar';
 import MedicineInventory from './components/Shop/MedicineInventory/MedicineInventoryComponent';
+import ShopSignUp from './components/Shop/SignUp/ShopSignUp';
+import HomePageComponent from './components/Shop/Home/HomePageComponent';
 
 function Shop() {
   return (    
@@ -9,6 +11,8 @@ function Shop() {
       <SideNavBar />
       <Switch>
           <Route path="/shop/inventory" component={MedicineInventory} />
+          <Route path="/shop/signup" component={ShopSignUp} />
+          <Route path="/shop/homepage" component={HomePageComponent} />
       </Switch>
     </div>
   );
