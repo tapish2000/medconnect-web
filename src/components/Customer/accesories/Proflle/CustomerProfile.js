95de7c91d5fc3933a197197672ce527e911b9851
rohit-cs18b029 <54976520+rohit-cs18b029@users.noreply.github.com>
2020-11-25 18:57:34 +0530

    fixed some bugs

---
+++ src/components/Customer/accesories/Proflle/CustomerProfile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CustomerProfile = () => {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col text-center">
+                        <h1>Customer Profile</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CustomerProfile;
