51825fb618429cfc9db9f7c8d14f7ab7df6a4a59
chirag2706 <43676985+chirag2706@users.noreply.github.com>
2020-11-25 15:39:09 +0530

    dashboard designed and safety stock has been added
    * profit and stock graph added
    
    * added heading to dashboard
    
    * safety stock table added
    
    * SafetyStockTable added
    
    * added progress bar
    
    * few css changes done
    
    * few bugs fixed
    
    * dashboard functionalities added along with bug fixes
    
    * few changes added
    
    * few css changes done
    
    Co-authored-by: rohit-cs18b029 <54976520+rohit-cs18b029@users.noreply.github.com>

--- src/Shop.js
+++ src/Shop.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import SideNavBar from './components/Shop/SideNavBar/Sidebar';
 import MedicineInventory from './components/Shop/MedicineInventory/MedicineInventoryComponent';
+import DashBoard from './components/Shop/DashBoard/DashBoardComponent'
+
 import ShopSignUp from './components/Shop/SignUp/ShopSignUp';
 import HomePageComponent from './components/Shop/Home/HomePageComponent';
 import Footer from './components/Shop/footer/FooterComponent'
+
 
 function Shop() {
   return (    
     <div>
       <SideNavBar />
       <Switch>
+          <Route exact path="/shop/inventory" component={MedicineInventory} />
+          <Route exact path="/shop/dashboard" component={DashBoard} />
           <Route path="/shop/inventory" component={MedicineInventory} />
           <Route path="/shop/signup" component={ShopSignUp} />
           <Route path="/shop/homepage" component={HomePageComponent} />
