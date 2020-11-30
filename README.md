010b202ddaa8aebdbbad4b4f912b5ed4f5d78022
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-11-30 20:15:23 +0530

    content updated

--- README.md
+++ README.md
@@ -11,36 +11,54 @@ Link to App - [https://github.com/shobhi1310/MedConnect](https://github.com/shob
  - Tag based searching for multiple medicines.
  - Cart system present.
  - Map view of stores present.
+
+## Features (R-2)
+- Get mail for confirmed bookings for the customer
+- View medicines only for shops available at locality who have it
+- Dashboard for shopowners
+- Stock and Inventory management
+- Manage reciepts
+- Preview uploaded prescriptions
+- Manage bookings from shop side
 ## Screenshots
 ![Home Page](screenshots/Home.png "Home Page")
+![Inventory Page](screenshots/shopHome.png "Shops Inventory Page")
 
 ## What's Special!!
 - 2 broad categories of medicines - Allopathic and Ayurvedic
 - Each category has been broken down further into Branded and Generic
 - Tag based searching for multiple medicines.
 - Cart system present.
+- Bookings possible from the cart.
 - Map view of stores present
 - Show brands and Daily use product
 - Current Bookings and past History for customer
 - Show Details of medicines
+- Manage inventory possible
+- Mailing done to the customers if booking succesful
+- Dashboard is introduced to view inhand and deadstocks.
 
 ## Steps to run the application
+### Customer
 - Visit the site at this [link](https://tapish2000.github.io/medconnect-web/#/)
 - In the top right corner, click on the user’s icon and select login if first time logging-in
-- For testing purposes use credentials
+- For testing purposes as customer use credentials
    - Username: `tapishojha2000@gmail.com`
    - Password: `taps123`
 - For searching medicines, enter medicine names for example Wikoryl, Propygenta in the search bar and select the tags generated and click the Search button
 - For booking any medicine from those click on the book medicine button
 - For exploring Ayurvedic and Allopathic products use the nav links
+### Shop Owner
+- Visit the site at this [link](https://tapish2000.github.io/medconnect-web/#/shop)
+- For testing puropses as shopowner use credentials
+   - Username: `meersameed@gmail.com`
+   - Password: `sameed`
+- For experimenting various featires check on the left hand side access the navbar.
+- For updating the inventory checkout the inventory sections
 
 ## Future Release
 - Ayurvedic alternatives to be recommended for any allopathic medicines searched
-- Medicine Booking functionality for customer
-- Medicine Search functionality in a particular Shop
-- Shop inventory management from shopkeeper’s end
-- Community forum and dashboards for posting daily news feed related to health
-- Security vulnerability will be removed in terms of password encryption, cookie storage, etc.
+- Security vulnerability will be removed in terms of password encryption, cookie-storage, etc.
 
 ## Request Changes
  1. Create an appropriate issue
--- src/components/Shop/Home/HomePageComponent.js
+++ src/components/Shop/Home/HomePageComponent.js
@@ -35,9 +35,11 @@ const HomePageComponent = () => {
                 <div className="row m-5">
                     <div className="col-md-6 text-center my-auto">
                         <h3>Manage Inventory</h3>
-                        <p>Leave the traditional way of manage inventory. The goal of inventory management systems is to
-                        know where your inventory is at any given time and how much of it you have in order to manage
-                           inventory levels correctly.</p>
+                        <p>
+                            Leave the traditional way of inventoroy mangament and move towards the new digital world of systematic management.
+                            Keep a track of what is present in your shop through our very user friendly UI. Also we have got added benefits of 
+                            updation and maintenace. 
+                        </p>
                     </div>
                     <div className="col-md-6 text-center">
                         <img className="rounded-circle" src="https://www.elite-bam.com/images/services/IM_1.png" width="250" height="200" alt="inventory" />
@@ -50,18 +52,20 @@ const HomePageComponent = () => {
                     </div>
                     <div className="col-md-6 text-center my-auto">
                         <h3>Sales and Profit</h3>
-                        <p>Leave the traditional way of manage inventory. The goal of inventory management systems is to
-                        know where your inventory is at any given time and how much of it you have in order to manage
-                           inventory levels correctly.</p>
+                        <p>
+                            The lifeline for any shop is through its sales and profits management. Experience a fully informative dashboard provided by
+                            us to get a first hand visual info of all your instock and deadstock items so that you can always be safe on trades and invest
+                            as per the market demand.
+                        </p>
                     </div>
                 </div>
 
                 <div className="row m-5">
                     <div className="col-md-6 text-center my-auto">
                         <h3>Booking Receipt</h3>
-                        <p>Leave the traditional way of manage inventory. The goal of inventory management systems is to
-                        know where your inventory is at any given time and how much of it you have in order to manage
-                           inventory levels correctly.</p>
+                        <p>We know that managing reciepts are tough. But we have got it covered for you completely. A fully proficient and rich user experience
+                            for letting you completely see all the ongoing bookings and even approve of these bookings.
+                        </p>
                     </div>
                     <div className="col-md-6 text-center">
                         <img className="rounded-circle" src="https://media.istockphoto.com/vectors/ticket-booking-office-linear-icon-sign-symbol-vector-on-isolated-vector-id866543798?k=6&m=866543798&s=170667a&w=0&h=ENOeVTm_cik9snUlisFqZawT0rbFvjVXY8sVRPZhLk4=" width="250" height="200" alt="booking-receipt" />
